eaddd8a01865a33a142a3c99fc06945753543c36
Noé Gambini <ngambini@neotech-solutions.fr>
2016-11-09 13:46:04 +0100

    [ADD] Better looking team page

--- app/team/team.component.ts
+++ app/team/team.component.ts
@@ -30,7 +30,8 @@ export class TeamComponent implements OnInit {
     }
 
     public isSelectedLanguage(locale: string) {
-        console.log(Languages[this.selectedLanguage]);
-        return Languages[this.selectedLanguage] === locale;
+        if (this._translateService.currentLang == 'en' && locale == 'English' || this._translateService.currentLang == 'fr' && locale == 'French')
+            return true;
+        return false;
     }
 }
